e5588e13153e05c174ea151a3ae0cd3782cd1c76
armelsteve1 <ouetchouaarmel@yahoo.com>
2024-03-04 16:20:26 +0100

    add resto screen

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -6,6 +6,7 @@ import CheckoutScreen from '../screens/CheckoutScreen';
 import MainTabNavigator from './MainTabNavigator';
 // import EditProfileScreen from '../screens/EditProfileScreen';
 // import AccountSettingsScreen from '../screens/AccountSettingsScreen';
+import QRCodeGenerator from '../screens/RestaurateurScreen'
 import AuthNavigator from './AuthNavigator';
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ export default function HomeNavigator() {
             <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
             <Stack.Screen name="Auth" component={AuthNavigator} />
             <Stack.Screen name="CheckoutScreen" component={CheckoutScreen} />
+            <Stack.Screen name='QrCode' component={QRCodeGenerator}/>
             {/* <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
             <Stack.Screen name="EditProfileScreen" component={EditProfileScreen} />
             <Stack.Screen name="AccountSettingsScreen" component={AccountSettingsScreen} /> */}
