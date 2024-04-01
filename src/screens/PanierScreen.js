0b96d1f1b224b7a2c7935af77847bdbd80d62ff0
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-04-01 17:38:10 +0200

    remove items when cart is empty

--- src/screens/PanierScreen.js
+++ src/screens/PanierScreen.js
@@ -9,17 +9,19 @@ import colors from '../component/configs/colors';
 import CartItems from '../component/CartItems';
 import { CartContext } from '../hooks/cartContext';
 import { useNavigation } from '@react-navigation/core';
+import ViewCart from '../component/ViewCart';
 
 
 const CartScreen = ({ route }) => {
-    const { total = 0, count = 0 } = route.params || {};
+
+    const { total = 0 } = route.params || {};
     const [modalVisible, setModalVisible] = useState(false);
     const [cartTotal, setCartTotal] = useState(total);
+    const [count, setCount] = useState(0);
     const { cartItems, setCartItems } = useContext(CartContext);
-
     const navigation = useNavigation();
 
-    const addOrder = () => {
+       const addOrder = () => {
         setModalVisible(false);
         navigation.navigate("CheckoutScreen", { total: cartTotal, count: count });
     };
@@ -29,15 +31,42 @@ const CartScreen = ({ route }) => {
             return acc + item.foods.reduce((foodAcc, food) => foodAcc + (food.price || 0), 0);
         }, 0);
         setCartTotal(newTotal);
+        updateCount();
     }, [cartItems]);
+
+    const updateCount = () => {
+        const totalCount = cartItems.reduce((acc, item) => acc + item.foods.length, 0);
+        setCount(totalCount);
+    };
+
+    const handleRemove = (id, resName, resImage) => {
+        const updatedCartItems = cartItems.map(item => {
+            if (item.resName === resName) {
+                const updatedFoods = item.foods.filter(food => food.id !== id);
+                if (updatedFoods.length > 0) {
+                    return { ...item, foods: updatedFoods };
+                } else {
+                    return null;
+                }
+            }
+            return item;
+        }).filter(Boolean); 
+    
+        if (updatedCartItems.length === 0) {
+            setCartItems([]);
+        } else {
+            setCartItems(updatedCartItems);
+        }
+    };
+
 
     return (
         <Screen style={tailwind`flex-1 bg-white`}>
             <AppHead title={`Votre Panier (${count})`} icon="basket-outline" />
             <View style={tailwind`flex-1`}>
-                <CartItems />
+                <CartItems cartItems={cartItems} handleRemove={handleRemove} updateCount={updateCount} />
             </View>
-            {!!count > 0 && (
+            {count > 0 && (
                 <View style={tailwind`flex-row justify-between items-center px-5 pb-5`}>
                     <View style={styles.totalContainer}>
                         <Text style={styles.total}>Total</Text>
